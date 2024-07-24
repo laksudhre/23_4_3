774b1cf144149d5a0b158a020474bbe5aa837e86
laksudhre <laksudhre@gmail.com>
2024-07-24 16:24:06 -0700

    refactor

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <map>
+#include <ranges>
 
 #define WAGONS_COUNT 10
 #define OPTIMAL_PASSENGERS 20
 #define CALL(func, param) { func(param); }
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define FOR_AUTO(it, param) for (auto& it : std::views::values(param))
 
 void userInput(std::map<int, int>& passengersCount) {
-    for (int i = 0; i < WAGONS_COUNT; i++) {
+    FOR(i, WAGONS_COUNT) {
         std::cout << "Enter the number of passengers on wagon " << (i + 1) << ": ";
         int passengers = 0;
         std::cin >> passengers;
@@ -16,16 +19,15 @@ void userInput(std::map<int, int>& passengersCount) {
 
 void show(const std::map<int, int>& passengers) {
     int lessOptimal = 0, moreOptimal = 0, passengersCount = 0;
-    for (auto const& pair : passengers) {
-        if (pair.second < OPTIMAL_PASSENGERS) {
+    FOR_AUTO(it, passengers) {
+        if (it < OPTIMAL_PASSENGERS) {
             ++lessOptimal;
         }
-        if (pair.second > OPTIMAL_PASSENGERS) {
+        if (it > OPTIMAL_PASSENGERS) {
             ++moreOptimal;
         }
-        passengersCount += pair.second;
+        passengersCount += it;
     }
-
     std::cout << "Wagons with less than optimal passenger count: " << lessOptimal << std::endl;
     std::cout << "Wagons with more than optimal passenger count: " << moreOptimal << std::endl;
     std::cout << "Total passengers count: " << passengersCount << std::endl;
