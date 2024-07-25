7604f2afd0be7c90f455f9a0f1cdd771a9b1b21d
laksudhre <laksudhre@gmail.com>
2024-07-24 18:15:47 -0700

    refactor

--- main.cpp
+++ main.cpp
@@ -4,21 +4,20 @@
 #define WAGONS_COUNT 10
 #define OPTIMAL_PASSENGERS 20
 #define CALL1(func, param) for (int i = 0; i < WAGONS_COUNT; ++i) { func(param, i); }
-#define CALL2(func, param1, param2, param3, param4) for (int i = 0; i < WAGONS_COUNT; ++i) { func(param1, param2, param3, param4, i); }
+#define CALL2(func, param1, param2, param3, param4) for (int i = 0; i < WAGONS_COUNT; ++i) { func(param2, param3, param4, param1[i]); }
 
-void userInput(std::map<int, int>& passengers, const int& i) {
-    std::cout << "Enter passengers count for wagon " << i + 1 << ": ";
-    std::cin >> passengers[i];
+void userInput(std::map<int, int>& passengers, const int& key) {
+    std::cout << "Enter passengers count for wagon " << key + 1 << ": ";
+    std::cin >> passengers[key];
 }
 
-void checkPassengers(const std::map<int, int>& passengers, int& total, int& less, int& more, const int& i) {
-    const int current = passengers.at(i);
-    if (current < OPTIMAL_PASSENGERS) {
+void checkPassengers(int& total, int& less, int& more, const int& value) {
+    if (value < OPTIMAL_PASSENGERS) {
         ++less;
-    } else if (current > OPTIMAL_PASSENGERS) {
+    } else if (value > OPTIMAL_PASSENGERS) {
         ++more;
     }
-    total += current;
+    total += value;
 }
 
 int main() {
